e6e78d26d1a5e9e659e4783b01de7e62e1e67b9f
Gbaddebo Motunrayo <gbadebomotunrayo1104@gmail.com>
2023-06-15 13:57:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdo.h>
+/**
+ * main -A programe that print a line unist the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		return (0);
+}
