b272d2b798d8a6e702140bd351324893d43c0a1d
Gbaddebo Motunrayo <gbadebomotunrayo1104@gmail.com>
2023-06-20 07:40:42 -0700

    1-main.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,12 @@
 #include "main.h"
-
 /**
  * print_alphabet - Make the alphabet
  * Retun: void
  */
-
 void print_alphabet(void)
-
 {
-
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
-
 	_putchar(c);
-
 	_putchar('\n');
-
 }
